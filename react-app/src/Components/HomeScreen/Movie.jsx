a389022de68cadaa6025d330f4bdf06904923d97
Juan <juangiasl@gmail.com>
2022-08-29 15:28:45 -0300

    Resolved conflicts

---
+++ react-app/src/Components/HomeScreen/Movie.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+
+import styles from "./Movie.module.css";
+
+import { ReactComponent as AddFavorite } from "../../assets/addFavorite.svg";
+import { ReactComponent as Share } from "../../assets/share.svg";
+import play from "../../assets/play.svg";
+import StarRate from "../UI/StarRate";
+
+import { selectors } from "../../store/slices/movies";
+
+function Movie({ movie, baseUrl, fileSize }) {
+  const genres = useSelector(selectors.getGenres);
+
+  const genreList = movie?.genre_ids
+    ?.filter((id) => genres.some((genre) => genre.id === id))
+    .map((genreId) => genres.find((genre) => genre.id === genreId)?.name);
+
+  const genrePlainText = genreList?.join(" • ");
+
+  return (
+    <div className={styles.poster}>
+      <img
+        src={`${baseUrl}${fileSize}${movie.poster_path}`}
+        alt={movie.title}
+        className={styles.image}
+      />
+      <div className={styles["image-overlay"]}>
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row items-center">
+            <div className="inline-block">
+              <AddFavorite fill="#aba2a2" />
+            </div>
+            <div className="inline-block ml-2">
+              <Share fill="#aba2a2" />
+            </div>
+          </div>
+          <div className="self-start">
+            <div className="border rounded-3xl border-[#aba2a2] text-xs w-16 h-7 flex justify-center items-center text-[#aba2a2] font-semibold sm:self-end self-start sm:mt-0 mt-4">
+              PG 13
+            </div>
+          </div>
+        </div>
+        <div className={styles["movie-title"]}>
+          {movie?.original_title?.length > 0
+            ? movie.original_title
+            : movie.name}
+        </div>
+        <div className="pt-3 flex">
+          <StarRate rate={movie?.vote_average} size="small" />
+        </div>
+        <div className="pt-3 text-white text-sm flex justify-between items-center">
+          <p className={styles["cut-text"]}>
+            {genrePlainText?.length > 0 ? genrePlainText : ""}
+          </p>
+          <div>
+            <img src={play} alt="Play" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  movie: PropTypes.objectOf(PropTypes.any).isRequired,
+  baseUrl: PropTypes.string.isRequired,
+  fileSize: PropTypes.string,
+};
+
+Movie.defaultProps = {
+  fileSize: "original",
+};
+
+export default Movie;
---
+++ react-app/src/Components/HomeScreen/MovieDetails.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+
+import centerDot from "../../assets/centerDot.svg";
+import directorLogo from "../../assets/directorLogo.svg";
+import { ReactComponent as AddFavorite } from "../../assets/addFavorite.svg";
+import watchTrailer from "../../assets/watchTrailer.svg";
+import imbd from "../../assets/imbd.svg";
+import volume from "../../assets/volume.svg";
+import StarRate from "../UI/StarRate";
+
+import { selectors } from "../../store/slices/movies";
+
+function MovieDetails({ openNav, selectedMovie }) {
+  const genres = useSelector(selectors.getGenres);
+
+  const movieYear = new Date(
+    selectedMovie?.release_date || selectedMovie?.first_air_date,
+  ).getFullYear();
+
+  const genreList = selectedMovie?.genre_ids
+    ?.filter((id) => genres.some((genre) => genre.id === id))
+    .map((genreId) => genres.find((genre) => genre.id === genreId)?.name);
+
+  const genrePlainText = genreList?.join(", ");
+
+  return (
+    <div
+      className={`mx-16 md:mx-36 xl:mx-64 ${
+        openNav ? "md:mt-24 mt-60" : "mt-24"
+      } transition-all duration-200 ease-in`}
+    >
+      <div className="flex">
+        <p className="text-white mr-2">{movieYear}</p>
+        <div className="relative top-2.5">
+          <img src={centerDot} alt="center-dot" />
+        </div>
+        <div className="ml-2">
+          <p className="text-white">{genrePlainText}</p>
+        </div>
+      </div>
+      <div className="flex justify-between mt-8 flex-col sm:flex-row">
+        <p className="w-3/4 sm:text-5xl text-4xl font-bold text-white">
+          {selectedMovie?.original_title?.length > 0
+            ? selectedMovie.original_title
+            : selectedMovie.name}
+        </p>
+        <div className="border rounded-3xl border-white sm:text-xl text-base sm:w-24 w-20 h-8 flex justify-center items-center text-white font-semibold sm:self-end self-start sm:mt-0 mt-4">
+          PG 13
+        </div>
+      </div>
+      <div className="mt-10 flex flex-col sm:flex-row">
+        <div className="bg-[#FF056C] border border-[#FF056C] rounded-lg w-16 h-8 flex justify-center items-center text-white sm:text-lg text-sm self-center">
+          2:30h
+        </div>
+        <div className="sm:ml-14 mx-auto sm:mr-4 sm:mt-0 mt-7 sm:mb-0 mb-3">
+          <img src={directorLogo} alt="director-logo" />
+        </div>
+        <div className="text-white text-lg self-center">Quentin Tarantino</div>
+        <div className="sm:ml-14 mx-auto sm:mt-0 mt-7 sm:mb-0 mb-3 self-center flex justify-center">
+          <StarRate rate={selectedMovie?.vote_average} size="normal" />
+        </div>
+      </div>
+      <p className="text-white mt-10 sm:w-3/4 w-auto sm:text-base text-sm">
+        {selectedMovie?.overview}
+      </p>
+      <div className="flex sm:flex-row flex-col sm:justify-between mt-8 sm:mt-28">
+        <div className="flex items-center flex-col sm:flex-row">
+          <div className="sm:ml-0 mx-auto sm:mr-4 sm:mt-0 mt-7 sm:mb-0 mb-3">
+            <AddFavorite fill="#92AAD7" />
+          </div>
+          <div className="text-[#92AAD7] text-lg self-center">Watch Later</div>
+          <div className="sm:ml-14 mx-auto sm:mr-3 sm:mt-0 mt-7 sm:mb-0 mb-3">
+            <img src={watchTrailer} alt="watch-trailer" />
+          </div>
+          <div className="text-[#92AAD7] text-lg self-center">
+            Watch Trailer
+          </div>
+          <div className="sm:ml-14 mx-auto sm:mr-3 sm:mt-0 mt-7 sm:mb-0 mb-3">
+            <img src={imbd} alt="imbd" />
+          </div>
+        </div>
+        <div className="self-center sm:mt-0 mt-7">
+          <img src={volume} alt="volume" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+MovieDetails.propTypes = {
+  openNav: PropTypes.bool.isRequired,
+  selectedMovie: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default MovieDetails;
